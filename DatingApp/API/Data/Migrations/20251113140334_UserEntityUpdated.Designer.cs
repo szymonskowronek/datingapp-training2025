f531cb500d86da58860cbd8025701bfedd9ea691
Szymon Skowronek <szymon.skowronek@globallogic.com>
2025-11-13 16:43:41 +0100

    Authorization and Authentication initial changes correct

--- DatingApp/API/Data/Migrations/20251113140334_UserEntityUpdated.Designer.cs
+++ DatingApp/API/Data/Migrations/20251113140334_UserEntityUpdated.Designer.cs
@@ -2,6 +2,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251113140334_UserEntityUpdated")]
+    partial class UserEntityUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
@@ -33,7 +36,7 @@ namespace API.Data.Migrations
                         .IsRequired()
                         .HasColumnType("BLOB");
 
-                    b.Property<byte[]>("PasswordSalt")
+                    b.Property<byte[]>("PaswordSalt")
                         .IsRequired()
                         .HasColumnType("BLOB");
 
