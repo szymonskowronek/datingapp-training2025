1ddfd779183e8dce7493928e5d966d01333e3f5d
Szymon Skowronek <szymon.skowronek@globallogic.com>
2025-11-24 15:20:24 +0100

    Types and registration feature added

---
+++ DatingApp/client/src/core/services/account-service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:5001/api/';
+  currentUser = signal<User | null>(null);
+
+  register(creds: RegisterCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap((user) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  login(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+      tap((user) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUser.set(user);
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+  logout() {
+    this.currentUser.set(null);
+    localStorage.removeItem('user');
+  }
+}
